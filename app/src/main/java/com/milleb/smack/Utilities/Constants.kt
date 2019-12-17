fe0477a98019caf1d6ba7c86a0c859b6757c8765
bezaluv <teklesenbet.filmon@yahoo.com>
2019-12-17 06:38:43 -0500

    lesson 11

--- app/src/main/java/com/milleb/smack/Utilities/Constants.kt
+++ app/src/main/java/com/milleb/smack/Utilities/Constants.kt
@@ -2,3 +2,4 @@ package com.milleb.smack.Utilities
 
 const val BASE_URL =  "https://intense-stream-49227.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
